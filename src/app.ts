47f6252ce1914fa9d42589d5652cb0b248ff41cf
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-12-12 14:41:09 +0100

    feat: project setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 app.use(express.json());
 app.use('/api', routes);
 
