21cee13fa0bc57a2b46afd2bc433acb2aeb9ffcd
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-12-12 15:21:34 +0100

    feat: project setup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,19 +16,20 @@ export const signup = async (req: Request, res: Response) => {
   }
 };
 
-export const login = async (req: Request, res: Response) => {
+
+export const login = async (req: Request, res: Response): Promise<Response> => {
   const { email, password } = req.body;
 
   try {
     const [user] = await knex('users').where({ email });
 
-    if (!user || !(await argon2.verify(password, user.password))) {
+    if (!user || !(await argon2.verify(user.password, password))) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-    res.status(200).json({ message: 'Login successful', token });
+    return res.status(200).json({ message: 'Login successful', token });
   } catch (error) {
-    res.status(500).json({ error: 'Error logging in', details: error });
+    return res.status(500).json({ error: 'Error logging in', details: error });
   }
 };
